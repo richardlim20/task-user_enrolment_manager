19f491d5fce05605c28bebadcd150bc9d0d21f5c
Richard Lim <s3901013@student.rmit.edu.au>
2025-06-04 15:09:08 +1000

    feat: implement enrol_user()

--- src/enrolment_manager.php
+++ src/enrolment_manager.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App;
 
 /**
  * Manage enrolments of users in courses.
  */
-class enrolment_manager {
+class enrolment_manager
+{
     /** @var array List of user objects. */
     private array $users = [];
 
@@ -20,7 +22,8 @@ class enrolment_manager {
      * @param array $users Array of user objects.
      * @param array $courses Array of course objects.
      */
-    public function __construct(array $users, array $courses) {
+    public function __construct(array $users, array $courses)
+    {
         // We populate this class with user and course data instead of relying on a database to simplify the application.
         $this->users = $users;
         $this->courses = $courses;
@@ -33,8 +36,40 @@ class enrolment_manager {
      * @param int $courseid ID of course object.
      * @return void
      */
-    public function enrol_user(int $userid, int $courseid): void {
-        // Implement this.
+    public function enrol_user(int $userid, int $courseid): void
+    {
+        //Looping through array to check if user or course exists, breaking once found.
+        $userExists = false;
+        foreach ($this->users as $user) {
+            if ($user->id === $userid) {
+                $userExists = true;
+                break;
+            }
+        }
+        $courseExists = false;
+        foreach ($this->courses as $course) {
+            if ($course->id === $courseid) {
+                $courseExists = true;
+                break;
+            }
+        }
+        //Throw error message if not found.
+        if (!$userExists || !$courseExists) {
+            throw new \InvalidArgumentException("Invalid user or course ID.");
+        }
+
+        // Check for duplicate enrolment and ends if duplicate
+        foreach ($this->enrolments as $enrolment) {
+            if ($enrolment['userid'] === $userid && $enrolment['courseid'] === $courseid) {
+                return;
+            }
+        }
+
+        //Adds enrolment
+        $this->enrolments[] = [
+            'userid' => $userid,
+            'courseid' => $courseid
+        ];
     }
 
     /**
@@ -44,7 +79,8 @@ class enrolment_manager {
      * @param int $courseid ID of user object.
      * @return void
      */
-    public function unenrol_user(int $userid, int $courseid): void {
+    public function unenrol_user(int $userid, int $courseid): void
+    {
         // Implement this.
     }
 
