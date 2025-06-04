43945b68fabfa28194972e53986e85419c07ec3c
Richard Lim <s3901013@student.rmit.edu.au>
2025-06-04 15:02:45 +1000

    test: add testcases
    Additional testcases added for
    - Duplicate enroll
    - Invalid User and Course ID
    - User unenroll

--- tests/enrolment_manager_test.php
+++ tests/enrolment_manager_test.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -9,7 +10,8 @@ use App\enrolment_manager;
 /**
  * Test the enrolment_manager class.
  */
-class enrolment_manager_test extends TestCase {
+class enrolment_manager_test extends TestCase
+{
 
     /** @var enrolment_manager Instance of enrolment_manager. */
     private enrolment_manager $manager;
@@ -19,7 +21,8 @@ class enrolment_manager_test extends TestCase {
      *
      * @return void
      */
-    protected function setUp(): void {
+    protected function setUp(): void
+    {
         $users = [
             new user(1, "Alice"),
             new user(2, "Bob")
@@ -34,11 +37,60 @@ class enrolment_manager_test extends TestCase {
     /**
      * Test a user can be enrolled.
      */
-    public function test_enrol_user_successfully(): void {
+    public function test_enrol_user_successfully(): void
+    {
         $this->manager->enrol_user(1, 100);
         $courses = $this->manager->get_user_courses(1);
         $this->assertCount(1, $courses);
     }
 
     // Add more tests: duplicate enrol, invalid ID, unenrol, etc.
+
+    /**
+     * Test duplicate enroll
+     */
+    public function testDuplicateEnrol(): void
+    {
+        $this->manager->enrol_user(1, 100);
+        $this->manager->enrol_user(1, 100);
+        $courses = $this->manager->get_user_courses(1);
+        //should still only show 1 course
+        $this->assertCount(1, $courses);
+    }
+
+    /**
+     * Test invalid ID
+     */
+
+    //Invalid UserID
+    public function testInvalidUserIdThrowsException(): void
+    {
+        $this->expectExceptionMessage('Invalid user or course ID.');
+        $this->manager->enrol_user(999, 100);
+    }
+
+    //Invalid CourseID
+    public function testInvalidCourseIdThrowsException(): void
+    {
+        $this->expectExceptionMessage('Invalid user or course ID.');
+        $this->manager->enrol_user(1, 999);
+    }
+    //Invalid UserID and CourseID
+    public function testInvalidUserCourseIdThrowsException(): void
+    {
+        $this->expectExceptionMessage('Invalid user or course ID.');
+        $this->manager->enrol_user(999, 999);
+    }
+
+    /**
+     * Test user unenroll
+     */
+    public function testUserUnenrol(): void
+    {
+        $this->manager->enrol_user(1, 100);
+        $this->manager->unenrol_user(1, 100);
+        $courses = $this->manager->get_user_courses(1);
+        //Should be 0 courses
+        $this->assertCount(0, $courses);
+    }
 }
